f50ab0bd377e3b4d7b28660d1862242d8ec35f93
Joshua Gregory <yoshig5114@gmail.com>
2023-05-28 15:20:52 -0400

    comleted

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 
-// Exercise 1 Section
-// console.log("EXERCISE 1:\n==========\n");
-// var sum = 0;
-// const numbers = [2, 22, 12, 17, 18, 39, 129];
-// function arraySum(arr){
+//Exercise 1 Section
+console.log("EXERCISE 1:\n==========\n");
+var sum = 0;
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+function arraySum(arr){
     
-//     for(let i = 0; i < arr.length; i++){
-//         sum = sum + arr[i];
-//     }
-//     return sum;
-// }
-// arraySum(numbers);
-// console.log(sum);
+    for(let i = 0; i < arr.length; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+arraySum(numbers);
+console.log(sum);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
-// let book = {};
-// book.title = "The Storyteller";
-// book.pages = 371;
-// book.readCount = 1;
-// book.info = function(){
-//     return `${book.title}, ${book.pages} pages, read ${book.readCount} time(s).`;
+let book = {};
+book.title = "The Storyteller";
+book.pages = 371;
+book.readCount = 1;
+book.info = function(){
+    return `${book.title}, ${book.pages} pages, read ${book.readCount} time(s).`;
 
-// }
-// console.log(book.info());
+}
+console.log(book.info());
 
 // exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// let sentence = "The quick brown fox jumps over the lazy dog";
-// let wordArr = [];
-// let reverseWord = [];
-// let joinedWord = "";
-// let splitSentence = sentence.split(" ");
-// let newSentence = "";
-// for (let i = 0; i < splitSentence.length; i++){
-//     wordArr = splitSentence[i].split("");
-//     reverseWord = wordArr.reverse(); 
-//     //console.log(reverseWord);
-//     joinedWord = reverseWord.join("");
-//     //console.log (joinedWord);
-//     newSentence = newSentence + joinedWord + " ";
-// }
-// console.log(newSentence)
+let sentence = "The quick brown fox jumps over the lazy dog";
+let wordArr = [];
+let reverseWord = [];
+let joinedWord = "";
+let splitSentence = sentence.split(" ");
+let newSentence = "";
+for (let i = 0; i < splitSentence.length; i++){
+    wordArr = splitSentence[i].split("");
+    reverseWord = wordArr.reverse(); 
+    //console.log(reverseWord);
+    joinedWord = reverseWord.join("");
+    //console.log (joinedWord);
+    newSentence = newSentence + joinedWord + " ";
+}
+console.log(newSentence)
 
 //exercise 4
 console.log("EXERCISE 4:\n==========\n");
@@ -52,8 +52,13 @@ let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
 let arr1 = csvData.split("\n");
 let columnHeads =arr1[0].split(",");
 let newArr = [];
-let arrObject = {};//needs to be in the loop ?
+
 for(let i = 1; i < arr1.length; i++) {
-    let rowSplit =
+    let arrObject = {};
+    let rowSplit = arr1[i].split(",");
+    arrObject[columnHeads[0]] = rowSplit[0];
+    arrObject[columnHeads[1]] = rowSplit[1];
+    newArr.push(arrObject);
 }
 
+console.log(newArr);
